9ce6329a0eb1e2a9bad7c46afd777b1800afa9d1
jason <quintanajason4004@gmail.com>
2023-06-01 01:59:10 -0400

    added some more socket events to help manage the state of the game

--- index.js
+++ index.js
@@ -10,7 +10,10 @@ const io = new Server(server, {
   },
 });
 
-let players = []
+let players = {}
+let prizePool = 0;
+let player1;
+let player2;
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', 'http://localhost:5173'); // Replace with your Vite client URL
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST');
@@ -20,12 +23,17 @@ app.use((req, res, next) => {
 
 io.on('connection', (socket) => {
   console.log(`${socket.id} connected`);
-  
+  socket.emit('newPlayer', 'wait')
+
   socket.on('player', (player) => {
     socket.username = player.name;
-    players.push(player)
+    players[socket.username] = player
     io.emit('players', players)
     console.log(`user ${socket.id} is now ${player.name}`)
+  })
+
+  socket.on('gameStart', () => {
+    io.emit('gameStart')
   })
 
   socket.on('chat message', (msg) => {
@@ -35,7 +43,36 @@ io.on('connection', (socket) => {
 
   socket.on('bet', (betAmmount) => {
     console.log(`${socket.username} betted: $${betAmmount}`)
+    if (prizePool === 0){
+      player1 = socket.username
+    } else {
+      player2 = socket.username
+    }
+    prizePool+=betAmmount
     io.emit('bet', betAmmount)
+  })
+
+  socket.on('diceRoll', (roll) => {
+    console.log(`${socket.username} rolled: ${roll}`)
+    io.emit('diceRoll', roll)
+  })
+
+  socket.on('yourNumber', (roll) => {
+    console.log(`${socket.username}'s number: ${roll}`)
+    io.emit('yourNumber', roll)
+  })
+
+  socket.on('winner', () => {
+    io.emit('bet', 0)
+    console.log(`${player1} just won $${prizePool}`)
+    socket.emit('prizeMoney', prizePool)
+    prizePool = 0;
+  })
+  socket.on('loser', () => {
+    io.emit('bet', 0)
+    console.log(`${player2} just won $${prizePool}`)
+    io.emit('prizeMoney', {winner: player2, prize: prizePool})
+    prizePool = 0;
   })
 
 });
